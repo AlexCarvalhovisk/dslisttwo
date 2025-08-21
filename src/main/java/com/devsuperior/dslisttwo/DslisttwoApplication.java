3507e7652288fd312a466d4b2a1ff27fa21057a4
Alex <adscarvalho@restinga.ifrs.edu.br>
2025-08-21 17:43:29 -0300

    Primeiro commit

---
+++ src/main/java/com/devsuperior/dslisttwo/DslisttwoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslisttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslisttwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslisttwoApplication.class, args);
+	}
+
+}
